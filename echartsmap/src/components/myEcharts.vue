4b17ac61596da96465794914b7bb380a465a511f
Rengf <1032387871@qq.com>
2019-07-08 13:13:24 +0800

    修改

--- echartsmap/src/components/myEcharts.vue
+++ echartsmap/src/components/myEcharts.vue
@@ -20,8 +20,7 @@ export default {
     drawLine(){
           var totalCost = [200000000, 200000000, 200000000,200000000,200000000,200000000, 200000000, 200000000,200000000,200000000]; //背景色比例
           var visits = [9336765, 10336765, 12336765,15944411,16336765,17336765,32336765,47336765,153367656,172336765]; //数值
-          var grade = ['缪超', '薛嘉运', '刘庆智','董洋','孔德娜','刘敏','梦蕾','猪刚鬣','孙悟空','唐三藏'];
-         
+          var grade = ['缪    超', '薛嘉运', '刘庆智','董    洋','孔德娜','刘    敏','梦    蕾','猪刚鬣','孙悟空','唐三藏'];
           var data = {
               grade: grade,
               totalCost: totalCost,
@@ -29,19 +28,12 @@ export default {
           };
           this.charts = echarts.init(document.getElementById('chart'));
           this.charts.setOption({
-            backgroundColor: '#ffffff',
-            grid: {
-              top:30,
-                left: 50,
-                right: 50,
-                bottom: 150,
-                containLabel:true
-
-            },
-            tooltip: {
+        backgroundColor:"rgba(255,255,255,255)",
+        tooltip: {
                 show: true,
                 axisPointer: {
                     link: {xAxisIndex: 'all'},
+                    type: 'cross',
                     label: {
                         backgroundColor: '#777'
                     }
@@ -50,100 +42,165 @@ export default {
                 textStyle:{
                   color:"#fff",
                   align:'left'
-                }
-            },
-            xAxis: {
-                show:false
-            },
-            yAxis: [{
-                type: 'category',
+                },
                 
-                axisTick: {
-                    show: false
-                },
-                axisLine: {
+            },
+        grid: {
+            top: '2%',
+            bottom: '1%',
+            left:'20%',
+            containLabel: true
+        },
+        xAxis: [
+            {
+                type: 'value',
+                axisLabel: {
                     show: false,
-                },
-                axisLabel: {
-                    margin:90,
+                    interval: 1,
                     textStyle: {
-                        align:'right',
-                        fontSize: 16,
-                        fontWeight: 'bold',
-                        color: '#171B1F'
-                    },
+                        color: '#fff',
+                        fontSize: 12
+                    }
                 },
-                data: data.grade
-            }, {
-                type: 'category',
-
                 axisLine: {
                     show: false
                 },
                 axisTick: {
                     show: false
                 },
-                axisLabel: {
-                    textStyle: {
-                        
-                        fontSize: 14,
-                        color: '#54AFEE',
-                    },
-                },
-            }, ],
-            series: [{
-                    type: 'pictorialBar',
-                    name: 'pictorial element',
-                    symbolSize: ['100%', '70%'],
-                    symbolPosition: 'center',
-                    z: 10,
-                    data: [{
-                        // Built-in type, can be 'circle', 'rect', 'roundRect', 'square', 'diamond', 'triangle', 'pin', 'arrow'.
-                        symbol: 'diamond',
-                    }, {
-                        // Image link.
-                        symbol: 'image://./../assets/logo.png',
-                    }, ]
-                },{
-                    type: 'bar',
-                    yAxisIndex: 1,
-                    itemStyle: {
-                        normal: {
-                            show: true,
-                            color: '#F0F0F0',
-                            barBorderRadius: 50,
-                        }
-                    },
-                    barWidth: '30%',
-
-                    data: data.totalCost
-                },
-                {
-
-                    type: 'bar',
-                    itemStyle: {
-                        normal: {
-                            show: false,
-                            color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
-                                offset: 0,
-                                color: '#6390F0'
-
-                            }, {
-                                offset: 1,
-                                color: '#25EBE9'
-
-                            }]),
-                            barBorderRadius: 50,
-                        }
-                    },
-                    barWidth: '30%',
-
-                    data: data.visits
+                splitLine: {
+                    show: false
                 }
 
-            ]
-          })
-    },
+            }
+        ],
+        yAxis:{
+                type: 'category',
+                data: ['0','1','2','3','4','5','6','7','8','9'],
+                axisLabel: {
+                    padding: [3, 4, 5, 6],
+                    color:"rgba(255,255,255,1)",
+                    formatter: function (value) {
+                            return '{' + value + '| }{value|'+grade[value]+'}';
+                    },
+                    rich: {
+                        value: {
+                            align: 'right',
+                            padding:[0,30,0,-75],
+                            fontSize:14
+                        },
+                        0: {
+                            height: 20,
+                            backgroundColor: {
+                                image: './static/10.png'
+                            },
+                        },
+                        1: {
+                            height: 20,
+                            align: 'right',
+                            backgroundColor: {
+                                image:  './static/9.png'
+                            },
+                        },
+                        2: {
+                            height: 20,
+                            align: 'right',
+                            backgroundColor: {
+                                image: './static/8.png'
+                            }
+                        },
+                        3: {
+                            height: 20,
+                            align: 'right',
+                            backgroundColor: {
+                                image: './static/7.png'
+                            }
+                        },
+                        4: {
+                            height: 20,
+                            align: 'center',
+                            backgroundColor: {
+                                image: './static/6.png'
+                            }
+                        },
+                        5: {
+                            height: 20,
+                            align: 'center',
+                            backgroundColor: {
+                                image: './static/5.png'
+                            }
+                        },
+                        6: {
+                            height: 20,
+                            align: 'center',
+                            backgroundColor: {
+                                image: './static/4.png'
+                            }
+                        },
+                        7: {
+                            height: 20,
+                            align: 'center',
+                            backgroundColor: {
+                                image: './static/3.png'
+                            }
+                        },
+                        8: {
+                            height: 20,
+                            align: 'center',
+                            backgroundColor: {
+                                image: './static/2.png'
+                            }
+                        },
+                        9: {
+                            height: 20,
+                            align: 'center',
+                            backgroundColor: {
+                                image: './static/1.png'
+                            }
+                        }
+                    }
+                },
+                axisTick: {
+                    show: false
+                },
+                axisLine: {
+                  show: false
+                },
+            },
+        series: [
+            {
+            type: 'bar',
+            barWidth: '20%',
+            barCategoryGap: '60%',
+            itemStyle: {
+                normal: {
+                  barWidth: 12,
+                    barBorderRadius:[10, 10, 10, 10],
+                    color: new echarts.graphic.LinearGradient(
+                            0, 0, 1, 0,
+                            [
+                                {offset: 0, color: '#048009'},
+                                {offset: 1, color: '#00EEF1'}
+                            ]
+                    ),
+                }
+            },
+            data:data.visits,
+            z:2
+        },{
+            type: 'bar',
+            barGap: '-100%',
+            itemStyle: {
+                normal: {
+                    barBorderRadius:[10, 10, 10, 10],
+                    color: '#aec314'
+                }
+            },
+            data:data.totalCost,
+            z:1
+        }]
+    })
+    }
   }
 }
 </script>
